3f3b88231807b27472a2409fc8652b4ff170fcc4
592fx <114221591+592fx@users.noreply.github.com>
2024-09-29 17:33:48 +0800

    Update jsb.js

--- jsb.js
+++ jsb.js
@@ -10,8 +10,6 @@
  * 自己抓包协议头上的Access-Token
 
  * 多账号换行或&隔开
-
- * 奖励：签到得安心币
  * 
  * ====================================
  *   
@@ -26,15 +24,27 @@ if(cookieString.length == 0) {
 
 const config = {
   headers:{
-    'Host': 'vip.ixiliu.cn',
+    'Host': 'xh-vip-api.a-touchin.com',
     'charset': 'utf-8',
-    "User-Agent": "Mozilla/5.0 (Linux; Android 12; M2007J22C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/116.0.0.0 Mobile Safari/537.36 XWEB/1160065 MMWEBSDK/20231002 MMWEBID/7003 MicroMessenger/8.0.43.2480(0x28002B51) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 MiniProgramEnv/android",
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 17_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.49(0x18003137) NetType/WIFI Language/zh_CN",
     "content-type": "application/json;charset=utf-8",
     "platform": "MP-WEIXIN",
+    "Enterprise-hash": "10009",
     "sid": "10009", 
-    "Referer": "https://servicewechat.com/wx9a2dc52c95994011/81/page-frame.html" 
+    "Referer": "https://servicewechat.com/wx9a2dc52c95994011/98/page-frame.html" 
   }
 };
+
+const config1 = {
+    headers: {
+    'Host': 'api.vshop.hchiv.cn',
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 17_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.49(0x18003137) NetType/WIFI Language/zh_CN",
+    "content-type": "application/json",
+    "Accept-Encoding": "gzip,compress,br,deflate",
+    "appenv": "test", 
+    "Referer": "https://servicewechat.com/wx5966681b4a895dee/8/page-frame.html" 
+  }
+}
 
 // -H "access-token: 6ZLRFshSMXVmoO33Jm5I6j0MT9nWQmrdBiM_UNU2ymusA3vr9qDhvqgrHokKpJoh" 
 
@@ -42,16 +52,10 @@ const config = {
 const cookies = cookieString.split('&');
 
 async function sign(ck) {
-  // 从青龙环境中读取参数
-  // 构造请求参数
-//   const requestData = {
-//     param1: param1,
-//     param2: param2
-//   };
   config.headers['access-token'] = ck;
   try {
     // 发送 POST 请求
-    const response = await axios.get('https://vip.ixiliu.cn/mp/sign/applyV2', config);
+    const response = await axios.get('https://xh-vip-api.a-touchin.com/mp/sign/applyV2', config);
 
     if(response.data.status == 200) {
         console.log('===签到成功====');
@@ -64,6 +68,35 @@ async function sign(ck) {
   }
 }
 
+async function vipsign() {
+    const ck = process.env.jsbvip;
+    const cks = ck.split('&');
+    for (let i = 1; i <= cks.length; i++) {
+         try {
+            const ss = cks[i - 1].split('#');
+            const cookie = ss[0];
+            const phone = ss[1];
+            const token = ss[2];
+            const timestamp = new Date().getTime();
+            config1.headers['Authorization'] = `Bearer ${token}`;
+            config1.headers['cookie'] = cookie;
+            const data = {
+                'appId': 'wx5966681b4a895dee',
+                'openId': 'true',
+                'shopId': '467028',
+                'shopNick': '',
+                'timestamp': `${timestamp}`,
+                'interfaceSource': 0,
+                'activityId': '154742'
+            };
+            const response = await axios.post(`https://api.vshop.hchiv.cn/jfmb/cloud/activity/sign/getSignPrize?sideType=3&mob=${phone}&appId=wx5966681b4a895dee&openId=true&shopId=467028&shopNick=wx5966681b4a895dee&timestamp=${timestamp}&unionid=`, data, config1);
+            console.log(response.data);
+        } catch (error) {
+
+        }
+    }
+}
+
 async function check(ck) {
   // 从青龙环境中读取参数
   // 构造请求参数
@@ -74,7 +107,7 @@ async function check(ck) {
   config.headers['access-token'] = ck;
   try {
     // 发送 POST 请求
-    const response = await axios.get('https://vip.ixiliu.cn/mp/user/info', config);
+    const response = await axios.get('https://xh-vip-api.a-touchin.com/mp/user/info', config);
 
     if(response.data.status == 200) {
         console.log('===查询成功====\n');
@@ -90,6 +123,8 @@ async function check(ck) {
 }
 
 const executeRequests = async () => {
+    await vipsign();
+    
 	for (let i = 1; i <= cookies.length; i++) {
 	    // console.log(cookies[i]);
 	    ck = cookies[i - 1];
